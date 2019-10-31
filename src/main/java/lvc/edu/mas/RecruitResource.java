09850830ec6bad41ae752d9f7f15f76d67cdf91f
Scott-Salerno74 <sws001@lvc.edu>
2019-10-31 09:48:06 -0400

    AddRecruit and Atomics

--- src/main/java/lvc/edu/mas/RecruitResource.java
+++ src/main/java/lvc/edu/mas/RecruitResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.io.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Root resource (exposed at "recruits" path)
@@ -15,7 +16,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Path("recruits")
 public class RecruitResource {
     private static ConcurrentHashMap<Integer,Recruit> recruitConcurrentHashMap = new ConcurrentHashMap<>();
-
+    private AtomicInteger id = new AtomicInteger(0);
     public static void storeRecruits(ConcurrentHashMap<Integer,Recruit> database) throws IOException {
         File file = new File("recruits.txt");
         FileOutputStream out = new FileOutputStream(file);
@@ -49,14 +50,16 @@ public class RecruitResource {
         return "Got it!";
     }
 
-    @Path("/{id}")
     @POST
     @Produces("application/json")
-    public void addCollege(String recruitJson) throws ParseException {
+    public void addCollege(String recruitJson) throws ParseException, IOException {
+        int newID = id.incrementAndGet();
+        id.set(newID);
         JSONParser parser = new JSONParser();
         JSONObject recruit = (JSONObject) parser.parse(recruitJson);
-        Recruit r = new Recruit(Integer.parseInt(recruit.get("id").toString()),recruit.get("fName").toString(),recruit.get("lName").toString(),recruit.get("schoolYear").toString(), recruit.get("sport").toString(), recruit.get("location").toString(), Integer.parseInt(recruit.get("recruitmentScore").toString()));
-        System.out.println(r.getlName() + " " + r.getLocation());
+        Recruit r = new Recruit(newID,recruit.get("fName").toString(),recruit.get("lName").toString(),recruit.get("schoolYear").toString(), recruit.get("sport").toString(), recruit.get("location").toString(), Integer.parseInt(recruit.get("recruitmentScore").toString()));
+        recruitConcurrentHashMap.put(newID,r);
+        System.out.println(r.getfName() + " " +r.getlName() + " " + r.getLocation() + " " + newID);
     }
     @Path("/{id}")
     @PUT
